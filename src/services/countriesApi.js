d9c05ed234e9a608f3249541b50facc28b248ef7
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2022-02-06 20:21:12 +0100

    update to the API
    Updated the api call to use async and await

--- src/services/countriesApi.js
+++ src/services/countriesApi.js
@@ -1,23 +1,4 @@
-// export default function getCountries() {
-//     return fetch('https://restcountries.com/v2/all')
-//         .then(res => res.json())
-// }
-
-
 export const getCountries = async() => {
     const res = await fetch('https://restcountries.com/v2/all');
     return res.json()
 };
-
-  // const [countryInfo, setCountryInfo] = useState([]);
-
-  // useEffect(() => {
-  //   let mounted = true;
-  //   getCountries()
-  //     .then(items => {
-  //       if(mounted) {
-  //         setCountryInfo(items)
-  //       }
-  //     })
-  //     return () => mounted = false;
-  // }, [])
