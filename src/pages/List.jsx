5eb767fe315460c3a91c44dd51229cdc76ee627a
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2022-02-09 01:29:26 +0100

    Update to List.jsx
    Implemented the search functionality

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,13 +1,32 @@
 import React, { useState, useEffect } from 'react'
-import SearchAndFilter from '../Components/SearchAndFilter'
+// import SearchAndFilter from '../Components/SearchAndFilter'
+import SearchBar from "../Components/Search/SearchBar"
+import Filter from "../Components/Filter/Filter"
 import Card from '../Components/Card'
 import { getCountries } from "../services/countriesApi"
 
 function List() {
 
-    const countries = [1,2,3,4,5,6]
-
     const [country, setCountryInfo] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+    const [searchedResults, setSearchedResults] = useState([]);
+
+    const searchItems = (searchValue) => {
+        setSearchInput(searchValue)
+        if (searchInput !== "") {
+            const searchedData = country.filter((item) => {
+                return Object.values(item).join("").toLowerCase().includes(searchInput.toLowerCase())
+            })
+            setSearchedResults(searchedData)
+        }
+        else {
+            setSearchedResults(country)
+        }
+    }
+
+    // const searchedData = country.filter((item) => {
+    //     return Object.values(item).join("").toLowerCase().includes(searchInput.toLowerCase())
+    // })
 
     useEffect(() => {
       let mounted = true;
@@ -19,30 +38,51 @@ function List() {
         })
         return () => mounted = false;
     }, [])
-
-    console.log(country)
     
     return (
         <>
             <div className="searchArea center-items">
-                <SearchAndFilter />
-                {/* <input placeholder="Enter Post Title" onChange={event => setQuery(event.target.value)} /> */}
+                <div className="searchAndFilter">
+                    <SearchBar 
+                        handleChange={(e) => searchItems(e.target.value)}
+                    />
+                    <Filter />
+                </div>
             </div>
 
             <div className="countries center-items">
                 <div className="card-container">
-                     {country.map((item) => <Card 
-                         key={item.alpha2Code}
-                         image={item.flags.png}
-                         country={item.name}
-                         population={item.population.toLocaleString()}
-                         region={item.region}
-                         capital={item.capital}
-                     />)}
-                </div>
+                {searchInput.length > 1 ? (
+                    searchedResults.map((item) => {
+                        return (
+                                <Card 
+                                    key={item.alpha2Code}
+                                    image={item.flags.png}
+                                    country={item.name}
+                                    population={item.population.toLocaleString()}
+                                    region={item.region}
+                                    capital={item.capital}
+                                />
+                           
+                        )
+                    })
+                ) : (
+                    country.map((item) => {
+                        return (
+                                <Card 
+                                    key={item.alpha2Code}
+                                    image={item.flags.png}
+                                    country={item.name}
+                                    population={item.population.toLocaleString()}
+                                    region={item.region}
+                                    capital={item.capital}
+                                />
+                        )
+                    })
+                )}
+                </div> 
             </div>
-        </>
-        
+        </> 
     )
 }
 
